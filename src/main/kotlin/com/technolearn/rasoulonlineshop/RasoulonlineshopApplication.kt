8b0711a095122a588cc46c449f70897ca8c04408
Rasoul Motie <rasoulmotie1380@gmail.com>
2023-03-31 17:41:50 +0330

    Init Project

---
+++ src/main/kotlin/com/technolearn/rasoulonlineshop/RasoulonlineshopApplication.kt
@@ -0,0 +1,11 @@
+package com.technolearn.rasoulonlineshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RasoulonlineshopApplication
+
+fun main(args: Array<String>) {
+	runApplication<RasoulonlineshopApplication>(*args)
+}
---
+++ src/test/kotlin/com/technolearn/rasoulonlineshop/RasoulonlineshopApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.technolearn.rasoulonlineshop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RasoulonlineshopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
